26479433969a2d0129a2228d706c337f59b25e12
Andrew Daly <andaly@fexco.com>
2020-01-09 11:15:53 +0000

    Merging

--- sumTwoSmallestNumbers.js
+++ sumTwoSmallestNumbers.js
@@ -1,13 +1,7 @@
-/**
- * @function sumTwoSmallestNumbers
- * @param  {type} numbers {array of numbers}
- * @return {type} {Sum of two lowest positive integers}
- */
 function sumTwoSmallestNumbers(numbers) {  
     //Code here
     var lowest1 = 0;
     var lowest2 = 0;
-    
     //get the lowest number
     lowest1 = Math.min.apply(Math, numbers);
     
@@ -16,7 +10,6 @@ function sumTwoSmallestNumbers(numbers) {
     if (index >= 0) {
       numbers.splice( index, 1 );
     }
-    
     //get the next lowest number
     lowest2 = Math.min.apply(Math, numbers);
     return lowest1+lowest2;
